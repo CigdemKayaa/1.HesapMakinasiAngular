fd7da961c79878431100878a7a969310432fac42
CigdemKayaa <49146485+CigdemKayaa@users.noreply.github.com>
2019-04-30 00:54:47 +0300

    Initial commit

---
+++ HesapMakinasi/Hesap/hesap-app.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module('hesapApp', []);
+app.controller('hesapController', function ($scope) {
+    $scope.Topla = function (a, b) {
+        $scope.sonuc = parseFloat(a) + parseFloat(b);
+    };
+    $scope.Cikar = function (a, b) {
+        $scope.sonuc = parseFloat(a) - parseFloat(b);
+    };
+    $scope.Carp = function (a, b) {
+        $scope.sonuc = parseFloat(a) * parseFloat(b);
+
+    };
+    $scope.Bol = function (a, b) {
+        $scope.sonuc = parseFloat(a) / parseFloat(b);
+
+    };
+    $scope.Tarih = function () {
+        console.log("merhaba");
+        $scope.tarih = new Date();
+        console.log(tarih);
+    }
+});
